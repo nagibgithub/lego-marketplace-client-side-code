9f4880083cd787c6980c3d7bc93bbd8c63f7285c
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-05-18 20:17:21 +0600

    fix the server connection error

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './App.css'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    loader: () => fetch('https://b7a11-toy-marketplace-server-side-nagibgithub.vercel.app/users')
   },
 ]);
 
